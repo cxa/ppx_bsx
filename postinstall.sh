1fc1f15bbdad4cb9baeac4b274874272dff87924
CHEN Xian-an <xianan.chen@gmail.com>
2019-05-26 19:01:46 +0800

    1.0.13

--- postinstall.sh
+++ postinstall.sh
@@ -1,5 +1,5 @@
 cd ../bs-platform
-sh scripts/buildocaml.sh
+node scripts/buildocaml.js
 cd ../ppx_bsx
-../bs-platform/vendor/ocaml/ocamlopt.opt -no-alias-deps -I +compiler-libs ocamlcommon.cmxa -I +compiler-libs str.cmxa ppx_bsx.ml -o ./bin/ppx_bsx.exe
+../bs-platform/ocaml/ocamlopt.opt -no-alias-deps -I +compiler-libs ocamlcommon.cmxa -I +compiler-libs str.cmxa ppx_bsx.ml -o ./bin/ppx_bsx.exe
 rm ppx_bsx.cmi ppx_bsx.cmx ppx_bsx.o
